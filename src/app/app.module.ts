20394bc40754c5b2fe3e255db1b077e9dea12856
Gianpietro Di Matteo <gianpietro.dimatteo@gmail.com>
2019-09-20 11:05:49 -0300

    Master/Detail Components Keeping all features in one component as the application grows will not be maintainable. You'll want to split up large components into smaller sub-components, each focused on a specific task or workflow. Moved the hero details into a separate, reusable HeroDetailComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- NgModel lives here
 
 // Some of the metadata is in the @NgModule decorators.
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent //Every component must be declared in exactly one NgModule.
+    HeroesComponent,
+    HeroDetailComponent //Every component must be declared in exactly one NgModule.
   ],
   imports: [
     BrowserModule,
