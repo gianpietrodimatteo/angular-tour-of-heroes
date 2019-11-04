8c49af4c93b5404a052c5b2180a2130b0ed11218
Gianpietro Di Matteo <gianpietro.dimatteo@gmail.com>
2019-11-04 12:37:25 -0300

    Cleaned up even the notes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { TutorialExcerptComponent } from './tutorial-excerpt/tutorial-excerpt.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'heroes', component: HeroesComponent },
-  { path: 'tutorial-excerpts', component: TutorialExcerptComponent }
+  { path: 'heroes', component: HeroesComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,11 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
-import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgModel lives here
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
-
-// related to the in memory web api, must create the InMemoryDataService with the mock
-// npm install angular-in-memory-web-api --save
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
-import { TutorialExcerptComponent } from './tutorial-excerpt/tutorial-excerpt.component';
 
 @NgModule({
   declarations: [
@@ -24,11 +20,13 @@ import { TutorialExcerptComponent } from './tutorial-excerpt/tutorial-excerpt.co
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent,
-    HeroSearchComponent,
-    TutorialExcerptComponent
+    HeroSearchComponent
   ],
   imports: [
     HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
       dataEncapsulation: false
     }),
